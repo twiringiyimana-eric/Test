dfac5a4025467fc444bb6a6d5dc9cd6170c89958
twiringiyimana-eric <khalifanineza42@gmail.com>
2023-01-24 17:28:25 +0200

    first commit

---
+++ klab_challenge.js
@@ -0,0 +1,73 @@
+
+
+const item = [
+    { name: 'Bike', price: 100 }, 
+    { name: 'TV', price: 200 }, 
+    { name: 'Album', price: 10 }, 
+    { name: 'Book', price: 5 }, 
+    { name: 'Phone', price: 500 }, 
+    { name: 'Computer', price: 1000 }
+];
+
+function Cheapest(array) {
+
+    let least = array[0].price;
+
+    for (let i=0; i<array.length; i++){
+
+        if (array[i].price <= least){
+            least = array[i].price;
+        }
+
+    }
+    
+     return `The cheapest item costs ${least}.`;
+}
+
+function Expensive(array) {
+    let most = array[0].price;
+
+    for (let i=0; i<array.length; i++){
+
+        if (array[i].price >= most){
+            most = array[i].price;
+        }
+
+    }
+
+    
+     return `The most expensive item costs ${most}.`;
+}
+
+function ProductTotal(array){
+    let total = 0;
+
+    for (let i=0; i<array.length; i++){
+
+        total += array[i].price;
+
+    }
+
+    return `The total cost is ${total}.`;
+}
+
+function TotalAboveTen(array){
+    let total = 0;
+
+    for (let i=0; i<array.length; i++){
+        if (array[i].price < 10){
+            continue;
+        }
+
+        else {
+            total += array[i].price;
+        }
+    }
+
+    return `The total cost of products above $10 is ${total}`;
+}
+
+console.log(Cheapest(item));
+console.log(Expensive(item));
+console.log(ProductTotal(item));
+console.log(TotalAboveTen(item));
